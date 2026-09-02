a398cecf1031161ad1e750dd456251e646a8560a
seb241181 <sebastien.medium.59@gmail.com>
2026-09-02 09:07:15 +0200

    feat(oracle): exposer le paiement Sandbox du suivi

--- api/oracle-interpret.js
+++ api/oracle-interpret.js
@@ -1,6 +1,9 @@
 /* global process */
 import { handleOracleTimeline } from '../lib/oracleTimeline.js'
+import { handleOracleFollowPayPal } from '../lib/oracleFollowPayPal.js'
 
 export default async function handler(req, res) {
+  const followAction = typeof req.query?.followAction === 'string' ? req.query.followAction.trim() : ''
+  if (followAction) return handleOracleFollowPayPal(req, res, followAction)
   return handleOracleTimeline(req, res)
 }

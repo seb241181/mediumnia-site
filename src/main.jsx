a2508505bd008ca6dd5c01b3aeac1d2cdabd3a1e
seb241181 <sebastien.medium.59@gmail.com>
2026-09-08 08:46:50 -0700

    Compte MediumIA global et profil Chronosphère
    Ajoute le compte global, la déconnexion, le profil Chronosphère mémorisé et les recommandations post-tirage avec Sébastien Seguin.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import GlobalAccount from './components/GlobalAccount.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <GlobalAccount />
   </StrictMode>,
 )

c5a1c435d133e550c3bf6607d709b7c489b6996d
seb241181 <sebastien.medium.59@gmail.com>
2026-09-17 13:34:12 +0200

    feat(rdv): route balance payment actions

--- api/rdv-config.js
+++ api/rdv-config.js
@@ -11,6 +11,7 @@ import { handleChronospherePayPal } from '../lib/chronospherePayPal.js'
 import { handleMediumiaAnalytics } from '../lib/mediumiaAnalytics.js'
 import { handleRdvDepositApi } from '../lib/rdvDepositApiHandler.js'
 import { handleRdvFullPaymentCreate } from '../lib/rdvFullPaymentApiHandler.js'
+import { handleRdvBalanceApi } from '../lib/rdvBalanceApiHandler.js'
 
 const SLUG_RE = /^[a-z0-9][a-z0-9-]{1,60}[a-z0-9]$/
 
@@ -25,6 +26,11 @@ const CONFIG_REQUIRED = (notice, practitioner = null, services = []) => ({
 
 export default async function handler(req, res) {
   res.setHeader('Cache-Control', 'no-store')
+
+  const rdvBalanceAction = req.query?.rdvBalanceAction
+  if (rdvBalanceAction) {
+    return handleRdvBalanceApi(req, res, rdvBalanceAction)
+  }
 
   const rdvDepositAction = req.query?.rdvDepositAction
   if (rdvDepositAction === 'createFull') {
